d0d07ccdac8a9719601bebc5d218ff03d22ecbed
butterbj <151415974+butterbj@users.noreply.github.com>
2023-11-19 14:30:48 -0500

    Add files via upload

---
+++ mult_div.py
@@ -0,0 +1,16 @@
+# Create prompt to tell user to input first number
+print('Input first number')
+# Create varable to store user input as integer
+num_1= int(input())
+# Create prompt to tell user to input second number
+print('Input second number')
+# Create varable to store user input as integer
+num_2= int(input())
+# Create variable and store the product of num_1 and num_2
+mult_1= num_1 * num_2
+# Creat variable and store num_1/num_2
+div_1= num_1 / num_2
+# Create output with discription of mult_1
+print('The product of the first * second numbers is', mult_1)
+# Create output with discription of div_1
+print('The dividend of the first number divided by the second number is', div_1)
---
+++ sum_diff.py
@@ -0,0 +1,16 @@
+# Create prompt to tell user to input first number
+print('Input first number')
+# Create varable to store user input as integer
+num_1= int(input())
+# Create prompt to tell user to input second number
+print('Input second number')
+# Create varable to store user input as integer
+num_2= int(input())
+# Create variable and store the sum of num_1 and num_2
+sum_1= num_1 + num_2
+# Create variable and store the difference of num_1 and num_2
+diff_1= num_1 - num_2
+# Create output with discription of sum_1
+print('The sum of the first and second numbers is', sum_1)
+# Create output with discription of dif_1
+print('The difference between the first number is', diff_1)
